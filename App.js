d47485c82232e0e52d2b251bdf50a71f000683e8
jihun8861 <126228828+jihun8861@users.noreply.github.com>
2023-08-02 01:24:45 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -5,31 +5,72 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목,b] = useState('여자 코트 추천');
-  let [글제목1,c] = useState('강남 우동맛집');
-  let [글제목2,d] = useState('파이썬독학');
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
+  let [따봉,따봉변경] = useState([0,0,0]);
+  let [modal,setModal] = useState();
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className="list">
-        <h4>{글제목}</h4>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
+
+{/*      <div className="list">
+        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <div className='list1'>
-        <h4>{글제목1}</h4>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <div className='list2'>
-        <h4>{글제목2}</h4>
+      <div className='list'>
+        <h4 onClick={()=>{setModal(true)}}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div>*/}
+
+      {
+        글제목.map(function(a,i){
+          return(
+            <div className="list" key="0">
+              <h4>{글제목[i]}
+              <span onClick={()=>{
+                let copy = [...따봉];
+                copy[i] = copy[i] +1;
+                따봉변경(copy)
+              }}>👍</span>{따봉[i]}
+              </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal === true ? <Modal/> : null
+      }
+
     </div>
   );
+}
+
+
+function Modal(){
+  return(
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
